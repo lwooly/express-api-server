c15f94f0edb25a9af79ca6e076a1ca5469f87f2b
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-12-02 11:44:03 +0000

    all api endpoints added

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,4 +1,6 @@
+const { query } = require('express');
 const Todo = require('../models/todo')
+const mongoose = require('mongoose');
 
 exports.getTodos = async function(req, res) {
     let query = {};
@@ -32,15 +34,29 @@ exports.updateTodo = async function(req, res) {
     const todoData = req.body;
     const queryId = req.params.id;
 
-    console.log(`query`, queryId)
-
     try {
         const todo = await Todo.findById(queryId)
         console.log(`found document`, todo instanceof mongoose.Document)
-        todo = todoData
+
+        console.log(todoData)
+        todo.name = todoData.name
+        todo.description = todoData.description
         await todo.save()
         return res.status(202).json(todo);
     }catch (err) {
+        res.status(500).send(err)
+    };
+}
+
+
+exports.deleteTodo = async function(req, res) {
+    const queryId = req.params.id;
+    try {
+        const todo = await Todo.findById(queryId)
+        await Todo.deleteOne(todo)
+        return res.status(200).json(todo)
+
+    } catch (err) {
         res.status(500).send(err)
     };
 }
--- routes/todos.js
+++ routes/todos.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router()
 
 const {
-    getTodos, addTodo, updateTodo
+    getTodos, 
+    addTodo, 
+    updateTodo, 
+    deleteTodo
 } = require("../controllers/todo.controller")
 
 router
  .get("/:id?", getTodos)
  .post("/", addTodo)
  .post("/:id", updateTodo)
+ .post("/delete/:id", deleteTodo)
 
  module.exports = router;
